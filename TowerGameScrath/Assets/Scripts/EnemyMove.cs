66c6e9b1ce810a4de1570743718951c7bd54ea85
Joker <729790146@qq.com>
2018-12-29 23:17:21 +0800

    极简的寻路完成

---
+++ TowerGameScrath/Assets/Scripts/EnemyMove.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMove : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float speed;
+    public Transform currentWayPoint;
+    public int waypointIndex;
+
+    int indexLength;
+    //public GameObject[] waypoints;
+    void Start()
+    {
+        waypointIndex = 0;
+        currentWayPoint = ManageWayPoints.waypoints[0];
+        indexLength = ManageWayPoints.waypoints.Length;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MoveTopWayPoints();
+    }
+
+    void MoveTopWayPoints()
+    {
+        if (waypointIndex >= indexLength)
+        {
+            Destroy(this.gameObject);
+            return;
+        }
+        else if (Mathf.Abs(Vector3.Distance(this.transform.position, currentWayPoint.position)) < 0.25 )
+        {
+            waypointIndex++;
+            currentWayPoint = ManageWayPoints.waypoints[waypointIndex];
+        }
+        else
+        {
+            Vector3 direction = Vector3.Normalize(transform.position - currentWayPoint.position);
+            transform.Translate(direction * speed * Time.deltaTime);
+        }
+    }
+}
---
+++ TowerGameScrath/Assets/Scripts/ManageWayPoints.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ManageWayPoints : MonoBehaviour
+{
+    public static Transform[] waypoints;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        GameObject ways = GameObject.FindGameObjectWithTag("waypoints");
+        waypoints = new Transform[ways.transform.childCount];
+        for (int i = 0; i < ways.transform.childCount; i++)
+        {
+            waypoints[i] = transform.GetChild(i);
+        }
+    }
+
+}
